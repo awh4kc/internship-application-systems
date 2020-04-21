ad1481770f75c28a7c5e0b0782baa2ca0951f3e8
awh4kc <awh4kc@virginia.edu>
2020-04-21 05:17:05 +0000

    timings

--- ping.cc
+++ ping.cc
@@ -30,17 +30,24 @@ void Ping::getip() {
 }
 
 void Ping::ping() {
-    struct sockaddr_in serve;
-    socklen_t serve_addr_size;
+    struct sockaddr_in server;
+    socklen_t server_addr_size;
     int transmitted = 0;
     int received = 0;
+    struct timeval start;
+    struct timeval stop;
+    double min_val = INT_MAX;
+    double max_val = 0.0;
+    double average = 0.0;
+    double std_dev = 0.0;
+    double sum = 0.0;
+    double var = 0.0;
 
     int ttl_val = 52;
     if (setsockopt(_sockfd, SOL_IP, IP_TTL, &ttl_val, (socklen_t)sizeof(ttl_val)) != 0) {
         fprintf(stderr, "Failed to modify TTL in socket options\n");
         return;
     }
-
     struct timeval timeout;
     bzero(&timeout, sizeof(timeout));
     timeout.tv_sec = 2;
@@ -50,18 +57,23 @@ void Ping::ping() {
     }
 
     int count = 0;
+    gettimeofday(&start, NULL);
     while (_count == 0 || count < _count) {
         count++;
+        struct timeval start_ping;
+        struct timeval stop_ping;
         struct icmphdr hdr;
         bzero(&hdr, sizeof(hdr));
 
         hdr.type = ICMP_ECHO;
         hdr.code = 0;
-        hdr.un.echo.id = getpid();
+        hdr.un.echo.id = htons(getpid());
         hdr.un.echo.sequence = htons(count);
         hdr.checksum = 0;
         hdr.checksum = checksum((short*)&hdr, sizeof(hdr));
         sleep(1);
+
+        gettimeofday(&start_ping, NULL);
         int packet_sent = 1;
         if (sendto(_sockfd, &hdr, sizeof(hdr), 0, (struct sockaddr*)&_server, sizeof(_server)) <= 0) {
 			fprintf(stderr, "Cannot send packet.\n");
@@ -69,25 +81,40 @@ void Ping::ping() {
         } else {
             transmitted++;
         }
-        serve_addr_size = sizeof(serve);
+        server_addr_size = sizeof(server);
         if (interrupt) {
             break;
         }
-        if (recvfrom(_sockfd, &hdr, sizeof(hdr), 0, (struct sockaddr*)&serve, &serve_addr_size) <= 0 && count > 1) {
+        if (recvfrom(_sockfd, &hdr, sizeof(hdr), 0, (struct sockaddr*)&server, &server_addr_size) <= 0 && count > 1) {
+            gettimeofday(&stop_ping, NULL);
             fprintf(stderr, "Didn't receive packet.\n");
         } else {
+            gettimeofday(&stop_ping, NULL);
             if (packet_sent) {
                 received++;
-                fprintf(stdout, "received");
+                double time = (((stop_ping.tv_sec - start_ping.tv_sec) * 1000000L + stop_ping.tv_usec) - start_ping.tv_usec)/1000.0;
+                min_val = std::min(min_val, time);
+                max_val = std::max(max_val, time);
+                sum += time;
+                average = sum / received;
+                var += ((time - average) * (time - average));
+                fprintf(stdout, "%d bytes from %s: icmp_seq=%d ttl=%d time=%0.1f ms\n", 64, _ip_addr, count, 52, time);
             }
         }
     }
+    gettimeofday(&stop, NULL);
+    double total_time = (((stop.tv_sec - start.tv_sec) * 1000000L + stop.tv_usec) - start.tv_usec)/1000.0;
+    average = sum/received;
+    std_dev = sqrt(var/received);
 
     if (interrupt) {
         transmitted--;
     }
 
     fprintf(stdout, "\n--- %s ping statistics ---\n", _host.c_str());
+    double packet_loss = 1.0 * (transmitted - received) / transmitted * 100;
+    fprintf(stdout, "%d packets transmitted, %d received, %0.0f%% packet loss, time %0.0fms\n", transmitted, received, packet_loss, total_time);
+    fprintf(stdout, "rtt min/avg/max/mdev = %0.3f/%0.3f/%0.3f/%0.3f ms\n", min_val, max_val, average, std_dev);
 
     if (close(_sockfd) != 0) {
         fprintf(stderr, "Failed to close socket\n");
